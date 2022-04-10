c94b4d8d7bdb44a4850e347a06c779b8e8362ce3
Eurrus <vpavithra@students.vnit.ac.in>
2022-04-10 17:35:35 +0530

    added basic courses implementation
    added basic functionality of courses

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,6 @@
 from django.db import models
-
+class Course(models.Model):
+    Name =models.CharField(max_length=100)
+    Price = models.IntegerField()
+    Duration=models.CharField(max_length=30)
 # Create your models here.
--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Course
+# Create your views here.
+def courses(request):
+    return render(request,'courses.html')
+def display(request):
+    if request.method == 'POST':
+            p= request.POST['search']
+            interns = Course.objects.filter(Name__icontains=p).exists()
+            if interns:
+                interns=Course.objects.filter(Name__icontains=p)
+                
+                return render(request,'display.html',{'interns':interns})
+            else:
+                return render(request,'error.html')
+                
+    else:
+          return render(request,'error.html')
 
 # Create your views here.
